c092e2ff19bf0091fd9a0450a23c312274bceca2
Neonunux <regis.leloup@colombbus.org>
2014-04-20 19:36:09 +0200

    Tangara/AdministrationBundle: renderView with Twig, creation of the controller, routing.yml configuration

--- src/Tangara/AdministrationBundle/Controller/DefaultController.php
+++ src/Tangara/AdministrationBundle/Controller/DefaultController.php
@@ -6,8 +6,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('TangaraAdministrationBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('TangaraAdministrationBundle:Default:index.html.twig');
     }
+	public function studentAction()
+    {
+        return $this->render('TangaraAdministrationBundle:Default:student.html.twig');
+    }
+	public function studentchiefAction()
+    {
+        return $this->render('TangaraAdministrationBundle:Default:studentchief.html.twig');
+    }
+	public function teacherAction()
+    {
+        return $this->render('TangaraAdministrationBundle:Default:teacher.html.twig');
+    }
+	public function adminAction()
+    {
+        return $this->render('TangaraAdministrationBundle:Default:admin.html.twig');
+    }
+
 }
