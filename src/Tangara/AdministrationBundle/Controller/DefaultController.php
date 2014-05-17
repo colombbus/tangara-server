aaf9144d28703bdf2da7b18c1457fe1f3871971b
Neonunux <regis.leloup@colombbus.org>
2014-05-17 17:58:30 +0200

    TangaraAdministrationtBundle: ajax request with json encode dataAction

--- src/Tangara/AdministrationBundle/Controller/DefaultController.php
+++ src/Tangara/AdministrationBundle/Controller/DefaultController.php
@@ -3,11 +3,39 @@
 namespace Tangara\AdministrationBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
-class DefaultController extends Controller
-{
-    public function indexAction()
-    {
+class DefaultController extends Controller {
+
+    public function indexAction() {
+        $request = $this->getRequest();
+
+        if ($request->isXmlHttpRequest()) {
+            $response = new JsonResponse();
+            $response->setData(array(
+                'data' => 121,
+                'data0' => 120,
+                'data1' => 125,
+                'data2' => 124,
+                'data3' => 129,
+            ));
+            return $response;
+        }
         return $this->render('TangaraAdministrationBundle:Default:index.html.twig');
+    }
+    public function dataAction() {
+        $request = $this->getRequest();
+
+        if ($request->isXmlHttpRequest()) {
+            $response = new JsonResponse();
+            $response->setData(array(
+                'data' => 121,
+                'data0' => 120,
+                'data1' => 125,
+                'data2' => 124,
+                'data3' => 129,
+            ));
+            return $response;
+        }
     }
 }
