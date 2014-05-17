14620087361ea2405052b45581560e94b7f0c5ac
Neonunux <regis.leloup@colombbus.org>
2014-05-17 18:01:18 +0200

    TangaraAdministrationtBundle: ajax request dataAction and routing In the correct controller

--- src/Tangara/AdministrationBundle/Controller/DefaultController.php
+++ src/Tangara/AdministrationBundle/Controller/DefaultController.php
@@ -8,19 +8,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 class DefaultController extends Controller {
 
     public function indexAction() {
-        $request = $this->getRequest();
-
-        if ($request->isXmlHttpRequest()) {
-            $response = new JsonResponse();
-            $response->setData(array(
-                'data' => 121,
-                'data0' => 120,
-                'data1' => 125,
-                'data2' => 124,
-                'data3' => 129,
-            ));
-            return $response;
-        }
         return $this->render('TangaraAdministrationBundle:Default:index.html.twig');
     }
     public function dataAction() {
