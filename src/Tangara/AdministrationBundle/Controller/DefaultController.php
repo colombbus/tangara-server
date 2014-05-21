d7bff61df159840eef15483fa9db12925d5c3549
Neonunux <regis.leloup@colombbus.org>
2014-05-21 14:23:18 +0200

    Filesystem creation directory

--- src/Tangara/AdministrationBundle/Controller/DefaultController.php
+++ src/Tangara/AdministrationBundle/Controller/DefaultController.php
@@ -4,12 +4,24 @@ namespace Tangara\AdministrationBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOException;
 
 class DefaultController extends Controller {
 
     public function indexAction() {
+        
+        $fs = new Filesystem();
+
+        try {
+            $fs->mkdir('/home/tangara/dir/');
+        } catch (IOException $e) {
+            echo "An error occured while creating your directory";
+        }
+        
         return $this->render('TangaraAdministrationBundle:Default:index.html.twig');
     }
+
     public function dataAction() {
         $request = $this->getRequest();
 
@@ -20,15 +32,21 @@ class DefaultController extends Controller {
                 'projectId' => 121,
                 'projectURL' => "http:\\apps.colombbus.org\tangara\files\121",
                 'screen' => array('width' => 1024,
-                                 'height' => 768),
+                    'height' => 768),
                 'nbFiles' => 5,
                 'files' => array('file0' => "pomme.gif",
-                                 'file1' => "arbre",
-                                 'file2' => "maison",
-                                 'file3' => "sol",
-                                 'file4' => "pomme.tgr"),
+                    'file1' => "arbre",
+                    'file2' => "maison",
+                    'file3' => "sol",
+                    'file4' => "pomme.tgr"),
             ));
             return $response;
         }
     }
+
+    public function getFileAction() {
+
+
+    }
+
 }
