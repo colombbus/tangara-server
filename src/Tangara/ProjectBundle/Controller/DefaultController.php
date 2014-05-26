32b8f19e9a73cc06b365977442e2f697ec185315
Neonunux <regis.leloup@colombbus.org>
2014-05-26 22:22:04 +0200

    TPBundle: upload managment

--- src/Tangara/ProjectBundle/Controller/DefaultController.php
+++ src/Tangara/ProjectBundle/Controller/DefaultController.php
@@ -4,37 +4,60 @@ namespace Tangara\ProjectBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOException;
+
 use Tangara\ProjectBundle\Form\ProjectType;
 use Tangara\ProjectBundle\Entity\Document;
 use Tangara\ProjectBundle\Entity\Project;
 
-class DefaultController extends Controller {
 
-    public function indexAction($projectName) {
-        
-        
+class DefaultController extends Controller {
+    public function indexAction() {
+        $id = $this->get('session');
         return $this->render('TangaraProjectBundle:Default:index.html.twig');
     }
 
-    public function addProjectAction() {
+    public function showAction($id) {
+        return $this->render('TangaraProjectBundle:Default:show.html.twig');
+    }
+    
+    public function addAction() {
+        $id = $this->container->get('security.context')->getToken()->getUser()->getId();
+        $rootDir = $this->get('kernel')->locateResource('@app/config.yml', null, true);
+        
+        echo $rootDir;
+        $fs = new Filesystem();
+        //$fs->copy($originFile, $targetFile)
+        
+        $fs->mkdir('C:\tangara\\' . $id);
+        //if ($fs->exists('/home/tangara/'..)){
+            
+        //}
+         if ($this->getRequest()->isMethod('POST')) {
+             echo 'request'. $this->getRequest();
+         }
+        
         $manager = $this->getDoctrine()->getManager();
         $project = new Project();
         //$project->setDateCreation(new \DateTime());
         $form = $this->createForm(new ProjectType(), $project);
 
-        return $this->render('TangaraProjectBundle:Default:addProject.html.twig', array(
-                    'form' => $form->createView()
+        return $this->render('TangaraProjectBundle:Default:add.html.twig', array(
+            'form' => $form->createView(),
+            'id' => $id
         ));
     }
 
     public function uploadAction() {
+        
+
         $document = new Document();
         $form = $this->createFormBuilder($document)
                 ->add('name')
                 ->add('file')
                 ->getForm()
         ;
-
         if ($this->getRequest()->isMethod('POST')) {
             $form->bind($this->getRequest());
             if ($form->isValid()) {
@@ -46,10 +69,8 @@ class DefaultController extends Controller {
                 //$this->redirect($this->generateUrl(...));
             }
         }
-
         return $this->render('TangaraProjectBundle:Default:upload.html.twig', array(
                     'form' => $form->createView()
         ));
     }
-
 }
