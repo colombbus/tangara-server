76366e06639b5cac875be92f6341e2a0bf003ad7
Neonunux <regis.leloup@colombbus.org>
2014-06-02 11:27:31 +0200

    Project: data and files action Project from Administration in the fileController

--- src/Tangara/AdministrationBundle/Controller/DefaultController.php
+++ src/Tangara/AdministrationBundle/Controller/DefaultController.php
@@ -12,39 +12,6 @@ class DefaultController extends Controller {
     public function indexAction() {
         return $this->render('TangaraAdministrationBundle:Default:homepage.html.twig');
     }
-    
-    public function createAction() {
-        return $this->render('TangaraAdministrationBundle:Default:create.html.twig');
-    }
-    
-    public function dataAction() {
-        $request = $this->getRequest();
-        //$id = $request->get('security.context')->getToken()->getUser()->getId();
-        if ($request->isXmlHttpRequest()) {
-            $response = new JsonResponse();
-            $response->setData(array(
-                'projectId' => 121,
-                'projectURL' => "http://apps.colombbus.org/tangara_ui/project/121",
-                'screen' => array('width' => 1024,
-                    'height' => 768),
-                'files' => array("niveau1.tgr","niveau2.tgr", "promeneur.tgr", "fin.tgr")
-            ));
-            return $response;
-        }
-    }
-
-    public function getFilesAction() {
-        $request = $this->getRequest();
-        //$id = $request->get('security.context')->getToken()->getUser()->getId();
-
-        if ($request->isXmlHttpRequest()) {
-            $response = new JsonResponse();
-            $response->setData(array(
-                'files' => array("niveau1.tgr","niveau2.tgr", "promeneur.tgr", "fin.tgr")
-            ));
-            return $response;
-        }
-    }
 
     public function localeAction() {
         $request = $this->getRequest();
