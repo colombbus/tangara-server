0010aee4761af74a9a25ed4b1abd4b736af192ce
Neonunux <regis.leloup@colombbus.org>
2014-07-07 13:59:49 +0200

    TangaraUserBundle: variable in fr corrected

--- src/Tangara/UserBundle/Controller/ProfileController.php
+++ src/Tangara/UserBundle/Controller/ProfileController.php
@@ -50,7 +50,7 @@ class ProfileController extends BaseController
         return $this->container->get('templating')
                 ->renderResponse('FOSUserBundle:Profile:show.html.'.$this->container->getParameter('fos_user.template.engine'), array(
             'user' => $user, 
-            'projets' => $list_projets)
+            'projects' => $list_projets)
                         );
     }
 }
