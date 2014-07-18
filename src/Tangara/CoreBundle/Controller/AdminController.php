16c1de52139a27bd69164ba68b09121808bc37d7
Neonunux <regis.leloup@colombbus.org>
2014-07-18 13:10:05 +0200

    merge with Francois branch

--- src/Tangara/CoreBundle/Controller/AdminController.php
+++ src/Tangara/CoreBundle/Controller/AdminController.php
@@ -9,4 +9,8 @@ class AdminController extends Controller {
     public function indexAction() {
         return $this->render('TangaraCoreBundle:Admin:homepage.html.twig');
     }
+    
+    public function testAction() {
+        return $this->render('TangaraCoreBundle:Admin:test.html.twig');
+    }
 }
