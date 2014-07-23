b1915f30dc46dea6ad2a695d9cac86affa94464f
Neonunux <regis.leloup@colombbus.org>
2014-07-23 20:13:05 +0200

    DefaultAdminController test case fill

--- src/Tangara/CoreBundle/Tests/Controller/DefaultAdminControllerTest.php
+++ src/Tangara/CoreBundle/Tests/Controller/DefaultAdminControllerTest.php
@@ -8,6 +8,13 @@ class DefaultControllerTest extends WebTestCase
 {
     public function testIndex()
     {
+        /*
+        $user = new User();
+        $user->setRole('ROLE_ADMIN');
+        $user->setUsername('tangara-admin');
+        $user->setPassword('password');
+        */
+        
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
