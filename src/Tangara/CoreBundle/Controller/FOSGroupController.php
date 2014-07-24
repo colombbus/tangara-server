75e4d0ea460720e511765fe70387b78d981d9eb6
Neonunux <regis.leloup@colombbus.org>
2014-07-24 09:35:31 +0200

    fix typo on two FOSxxxController

--- src/Tangara/CoreBundle/Controller/FOSGroupController.php
+++ src/Tangara/CoreBundle/Controller/FOSGroupController.php
@@ -4,35 +4,28 @@ namespace Tangara\CoreBundle\Controller;
 
 //use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
-
+use Symfony\Component\HttpFoundation\Request;
 use FOS\UserBundle\Controller\GroupController as BaseController;
 
-class FOSGroupController extends BaseController
-{
- 
+class FOSGroupController extends BaseController {
+
     public function listAction() {
-        
-        //effectue les instructions du parent
         $response = parent::registerAction();
-        
+
         //puis redirection vers le controller de tangara-ui
         return $this->redirect($this->generateUrl('tangara_core_group_list'));
-        
     }
 
-    public function newAction(\Symfony\Component\HttpFoundation\Request $request) {
-        
-        $reponse = parent::newAction($request);
-        
-        return $reponse;
+    public function newAction(Request $request) {
+        $response = parent::newAction($request);
+
+        return $response;
     }
 
-    public function editAction(\Symfony\Component\HttpFoundation\Request $request, $groupName) {
-        
-        $reponse = parent::editAction($request, $groupName);
+    public function editAction(Request $request, $groupName) {
+        $response = parent::editAction($request, $groupName);
 
-        return $reponse;
-        
+        return $response;
     }
-    
+
 }
--- src/Tangara/CoreBundle/Controller/FOSProfileController.php
+++ src/Tangara/CoreBundle/Controller/FOSProfileController.php
@@ -2,17 +2,18 @@
 
 namespace Tangara\CoreBundle\Controller;
 
-
 use FOS\UserBundle\Controller\ProfileController as BaseController;
+//use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Component\HttpFoundation\Request;
 
-class FOSProfileController extends BaseController
-{
+class FOSProfileController extends BaseController {
     /*
-     * Si on doit rajouter des instructions en plus,
-     * alors on creer les actions
-     * puis on fait les instruction du parent()
-     * et on fait une redirection vers le controller TangaraCoreBundle:Profile
-     * et dans le controller TangaraCoreBundle:Profile on rajoute les instructions
-     * et toujours dans le controller TangaraCoreBundle:Profile on fait un render le template
+     * Add controllers here if needed
+     * 
+     * public function newAction(Request $request) {
+     *    $reponse = parent::newAction($request);
+     *    return $response;
+     * }
+     * 
      */
 }
