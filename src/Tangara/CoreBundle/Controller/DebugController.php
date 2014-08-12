13748199ad8be023f1d6c9f67deda2cf86e835e8
elise <elise@portifris54>
2014-08-12 08:36:16 -0400

    Ajax set session created To avoid complicated settings, there's now a request to set project_id in session (only in dev environment)

--- src/Tangara/CoreBundle/Controller/DebugController.php
+++ src/Tangara/CoreBundle/Controller/DebugController.php
@@ -4,11 +4,21 @@ namespace Tangara\CoreBundle\Controller;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
+
+//use Symfony\Component\HttpFoundation\Response;
 
 class DebugController extends Controller {
-    public function ajaxAction(){
+
+    public function ajaxAction() {
         return $this->render('TangaraCoreBundle:Debug:ajax_debug.html.twig');
+    }
+
+    public function setSessionAction($project_id) {
+        $request = $this->getRequest();
+        $session = $request->getSession();
+        $projectId = $session->set('projectid',$project_id);
+        $jsonResponse = new JsonResponse();
+        return $jsonResponse->setData(array('projectid' => $project_id));
     }
 
 }
