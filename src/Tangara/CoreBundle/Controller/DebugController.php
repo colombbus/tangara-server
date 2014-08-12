269c8b1c1aac0c2a36ec1960266a7ef5230632d7
elise <elise@portifris54>
2014-08-12 07:35:19 -0400

    Ajax page moved from 'Default' to 'Debug' directory

--- src/Tangara/CoreBundle/Controller/DebugController.php
+++ src/Tangara/CoreBundle/Controller/DebugController.php
@@ -5,11 +5,10 @@ namespace Tangara\CoreBundle\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
-use Tangara\CoreBundle\Entity\Project;
 
-class DefaultController extends Controller {
-
-    public function indexAction() {
-        return $this->render('TangaraCoreBundle:Default:homepage.html.twig');
+class DebugController extends Controller {
+    public function ajaxAction(){
+        return $this->render('TangaraCoreBundle:Debug:ajax_debug.html.twig');
     }
+
 }
