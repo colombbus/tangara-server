dadf2a5caa16f27911d841de22508a98984162d6
bonpra <benoit.prady@colombbus.org>
2014-09-02 18:39:23 +0200

    Fixed ajax method to handle programs and resources from tangara js

--- src/Tangara/CoreBundle/Entity/DocumentRepository.php
+++ src/Tangara/CoreBundle/Entity/DocumentRepository.php
@@ -19,6 +19,59 @@ class DocumentRepository extends EntityRepository {
         return $Docs;
     }
     
+    public function getAllProjectPrograms($projectId) {
+        $query = $this->createQueryBuilder('a')
+                ->where('a.ownerProject = :projectId')
+                ->andWhere('a.program = true')
+                ->setParameter('projectId', $projectId);
+        
+        $programs = $query->getQuery()->getResult();
+        
+        return $programs;
+        
+    }
     
+    public function getAllProjectResources($projectId) {
+        $query = $this->createQueryBuilder('a')
+                ->where('a.ownerProject = :projectId')
+                ->andWhere('a.program = false')
+                ->setParameter('projectId', $projectId);
+        
+        $resources = $query->getQuery()->getResult();
+        
+        return $resources;
+        
+    }
+
+    public function getProjectProgram($projectId, $name) {
+        $query = $this->createQueryBuilder('a')
+                ->where('a.ownerProject = :projectId')
+                ->andWhere('a.program = true')
+                ->andWhere('a.path = :name')
+                ->setParameters(array('projectId'=> $projectId, 'name'=> $name));
+        try {
+            $program = $query->getQuery()->getSingleResult();
+        } catch (\Doctrine\Orm\NoResultException $e) {
+            $program = false;
+        }
+        
+        return $program;
+    }
+
+    public function getProjectResource($projectId, $name) {
+        $query = $this->createQueryBuilder('a')
+                ->where('a.ownerProject = :projectId')
+                ->andWhere('a.program = false')
+                ->andWhere('a.path = :name')
+                ->setParameters(array('projectId'=> $projectId, 'name'=> $name));
+        try {
+            $resource = $query->getQuery()->getSingleResult();
+        } catch (\Doctrine\Orm\NoResultException $e) {
+            $resource = false;
+        }
+        
+        return $resource;
+    }
+
     
 }
