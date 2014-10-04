fcc6df1ecabca77504ca24544f794511c2a3ca99
bonpra <benoit@192.168.0.13>
2014-10-05 00:10:37 +0200

    Merged modifs from DocumentManager

--- src/Tangara/CoreBundle/Manager/FileManager.php
+++ src/Tangara/CoreBundle/Manager/FileManager.php
@@ -18,7 +18,7 @@
  */
 
 /**
- * Description of DocumentManager
+ * Description of FileManager
  *
  * @author Régis
  */
@@ -28,16 +28,19 @@ namespace Tangara\CoreBundle\Manager;
 use Doctrine\ORM\EntityManager;
 use Tangara\CoreBundle\Manager\BaseManager;
 use Tangara\CoreBundle\Entity\File;
+use Symfony\Component\Filesystem\Filesystem;
 
 class FileManager extends BaseManager {
 
     protected $em;
+    protected $pm;
     private $directory;
     private $allowedMimeTypes;
     private $maxResourceSize;
 
-    public function __construct(EntityManager $em, $allowed, $size) {
+    public function __construct(EntityManager $em, ProjectManager $pm, $allowed, $size) {
         $this->em = $em;
+        $this->pm = $pm;
         $this->directory = '/home/tangara';
         foreach ($allowed as $type=>$values) {
             foreach ($values as $value) {
@@ -73,6 +76,53 @@ class FileManager extends BaseManager {
     public function getRepository() {
         return $this->em->getRepository('TangaraCoreBundle:File');
     }
+    
+    public function remove(File $file) {
+        // Remove database entry
+        $this->em->remove($file);
+        $this->em->flush();
+
+        // Remove file from project
+        $project = $file->getProject();
+        $this->pm->removeFile($project, $file);
+
+        // Remove files
+        $projectPath = $this->pm->getProjectPath($project);
+        $fs = new Filesystem();
+        if ($file->getProgram()) {
+            // File is a program
+            $codePath = $projectPath. "/".$file->getPath()."_code";
+            $statementsPath = $projectPath . "/".$file->getPath()."_statements";
+            if ($fs->exists($codePath)) {
+                $fs->remove($codePath);
+            }
+            if ($fs->exists($statementsPath)) {
+                $fs->remove($statementsPath);
+            }
+        } else {
+            // File is not a program
+            $filePath = $projectPath. "/".$file->getPath();
+            if ($fs->exists($filePath)) {
+                $fs->remove($filePath);
+            }
+        }
+    }
+
+    public function updateProgram(File $program, $code, $statements) {
+        // find corresponding project
+        $project = $program->getProject();
+        $projectPath = $this->pm->getProjectPath($project);
+        
+        $codePath = $projectPath . "/".$program->getPath()."_code";
+        $statementsPath = $projectPath . "/".$program->getPath()."_statements";
+
+        file_put_contents($codePath, $code, LOCK_EX);
+        file_put_contents($statementsPath, $statements, LOCK_EX);
+        
+        // update project
+        $this->pm->updateFile($project, $program);
+    }
+
     
     public function checkResource($file) {
         // Check upload
