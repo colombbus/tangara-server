37cab032da7fc763d4f98d8f417c1296fed9eaa6
Benoit <Benoit@PORTABLE-10W8.pn.colombbus.org>
2015-05-26 17:38:06 +0200

    Removed unuser use statements

--- src/Tangara/CoreBundle/Form/Type/SearchType.php
+++ src/Tangara/CoreBundle/Form/Type/SearchType.php
@@ -1,14 +1,8 @@
 <?php
 namespace Tangara\CoreBundle\Form\Type;
 
-use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-use Symfony\Component\Security\Core\SecurityContext;
-use Tangara\CoreBundle\Manager\ProjectManager;
 
 class SearchType extends AbstractType {
     
