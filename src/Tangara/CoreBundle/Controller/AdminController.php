1727acca0b9eb358bb74f33eb6ac8f64aae49527
Benoit <Benoit@PORTABLE-10W8.pn.colombbus.org>
2015-05-26 17:43:45 +0200

    Tidy up

--- src/Tangara/CoreBundle/Controller/AdminController.php
+++ src/Tangara/CoreBundle/Controller/AdminController.php
@@ -1,8 +1,7 @@
 <?php
 
 /**
- * Description of ProfileController
- * @author badr
+ * Description of AdminController
  */
 
 namespace Tangara\CoreBundle\Controller;
@@ -12,9 +11,6 @@ use Tangara\CoreBundle\Form\Type\SearchType;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdminController extends TangaraController {
-
-    public $val;
-    public $user;
 
     public function indexAction() {
         return $this->renderContent('TangaraCoreBundle:Admin:index.html.twig', 'profile', array());
@@ -46,18 +42,17 @@ class AdminController extends TangaraController {
         $request = $this->getRequest();
         if ($request->isMethod('POST')) {
             $form->bind($this->get('request'));
-            $user = $this->getDoctrine()
+            $searchedUsers = $this->getDoctrine()
                     ->getEntityManager()
                     ->getRepository('TangaraCoreBundle:User')
                     ->searchData($form['search']->getData());
-            $this->val = $user;
-            $session = $request->getSession()->set('val', $this->val);
+            $request->getSession()->set('searchedUsers', $searchedUsers);
         } elseif ($request->isMethod('GET')) {
-            $user = $request->getSession()->get('val', $this->val);
+            $searchedUsers = $request->getSession()->get('searchedUsers');
         } else {
             throw $this->createNotFoundException('404 Not found');
         }
-        $users = $this->get('knp_paginator')->paginate($user, $this->get('request')->query->get('page', 1), 2);
+        $users = $this->get('knp_paginator')->paginate($searchedUsers, $this->get('request')->query->get('page', 1), 2);
         return $this->renderContent('TangaraCoreBundle:Admin:users.html.twig', 'profile', array('users' => $users));
     }
 
@@ -83,27 +78,5 @@ class AdminController extends TangaraController {
             return $this->renderContent('TangaraCoreBundle:Admin:users_ajax.html.twig', 'profile', array('form' => $form->createView()));
         }
     }
-
-
-
-//
-//    public function autoshowAction() {
-//        $form = $this->createForm(new SearchType());
-//        return $this->renderContent('TangaraCoreBundle:Admin:autocomplete.html.twig', 'profile', array('form' => $form->createView()));
-//    }
-//
-//    public function autocompleteAction() {
-//        $request = $this->get('request');
-//        if ($request->isXmlHttpRequest()) {
-//            $data = $request->request->get('user');
-//            $user = $this->getDoctrine()
-//                    ->getEntityManager()
-//                    ->getRepository('TangaraCoreBundle:User')
-//                    ->autocompleteData($data);
-//            $response = new Response(json_encode($user));
-//            $response->headers->set('Content-Type', 'application/json');
-//            return $response;
-//        }
-//    }
 
 }
