07b50594a804eba17f74a06093174fbb3bfb6bea
Benoit <Benoit@PORTABLE-10W8.pn.colombbus.org>
2015-06-01 14:52:24 +0200

    Wrote migration to init ACL DB from existing users and projects

--- app/DoctrineMigrations/Version20150529175721.php
+++ app/DoctrineMigrations/Version20150529175721.php
@@ -15,8 +15,7 @@ class Version20150529175721 extends AbstractMigration
      */
     public function up(Schema $schema)
     {
-        // add Project ACL class
-        $this->addSql("INSERT INTO `acl_classes` (`id`, `class_type`) VALUES (NULL, 'Tangara\CoreBundle\Entity\Project');");
+        // perform nothing: ACL init has to be done through symfony command init:acl
     }
 
     /**
@@ -25,20 +24,35 @@ class Version20150529175721 extends AbstractMigration
     public function down(Schema $schema)
     {
         // remove Project ACL class
-        $this->addSql("DELETE FROM `acl_classes` WHERE `id` = 1");
-
+        $this->addSql("DELETE FROM acl_classes");
+        $this->addSql("DELETE FROM acl_security_identities");
+        $this->addSql("DELETE FROM acl_object_identities");
+        $this->addSql("DELETE FROM acl_object_identity_ancestors");
+        $this->addSql("DELETE FROM acl_entries");
     }
     
     public function postUp(Schema $schema) {
+        // insert project class
+        $this->connection->insert("acl_classes", ['class_type'=>"Tangara\\CoreBundle\\Entity\\Project"]);
+        $classId = $this->connection->lastInsertId();
+        // scan users
         $result = $this->connection->query("SELECT username, home_id from users");
         while ($row = $result->fetch()) {
-            // insert User object
-            $class = "Tangara\Core\Entity\User-".$row['username'];
-            $result2 = $this->connection->query("INSERT INTO `acl_object_identities` (`class_id`, `object_identifier`, `entries_inheriting`) VALUES (NULL, $hId, 1);");
-            // insert Project object
             $hId = $row['home_id'];
-            $this->connection->query("INSERT INTO `acl_object_identities` (`class_id`, `object_identifier`, `entries_inheriting`) VALUES (NULL, $hId, 1);");
-            $this->connection->query("INSERT INTO `acl_object_identities` (`class_id`, `object_identifier`, `entries_inheriting`) VALUES (NULL, $id, 1);");
+            if (isset($hId)) {
+                // insert User object
+                $class = "Tangara\\CoreBundle\\Entity\\User-".$row['username'];
+                $this->connection->insert("acl_security_identities", ['identifier'=>$class, 'username'=>1]);
+                //$this->connection->query("INSERT INTO acl_security_identities (identifier, username) VALUES (\"$class\", 1);");
+                $securityId = $this->connection->lastInsertId();
+                // insert Project object
+                $this->connection->insert("acl_object_identities", ['class_id'=>$classId, 'object_identifier'=>$hId, 'entries_inheriting'=>1]);
+                $objectId = $this->connection->lastInsertId();
+                //$this->connection->query("INSERT INTO acl_object_identity_ancestors (object_identity_id, ancestor_id) VALUES ($objectId, $objectId);");
+                $this->connection->insert("acl_object_identity_ancestors", ['object_identity_id'=>$objectId, 'ancestor_id'=>$objectId]);
+                // insert entry
+                $this->connection->insert("acl_entries", ['class_id'=>$classId, 'object_identity_id'=>$objectId, 'security_identity_id'=>$securityId, 'ace_order'=>0, 'mask'=>128, 'granting'=>1, 'granting_strategy'=>'all', 'audit_success'=>0, 'audit_failure'=>0]);
+            }
         }
     }
 }
